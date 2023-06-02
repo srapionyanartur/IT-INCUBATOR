f615f7d921318401f8cefe81e6955aba21fbf440
srapionyanartur <arthur.srapionyan@gmail.com>
2023-06-02 12:06:17 +0400

    commit_2

---
+++ JS/06.js
@@ -0,0 +1,2 @@
+var myFirstName = "Artur";
+var myLastName = "Srapionyan";
---
+++ JS/08.js
@@ -0,0 +1,9 @@
+// Variable declarations
+var studyCapVar;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+studyCapVar = 10;
+properCamelCase = "A String";
+TitleCaseOver = 9000;
